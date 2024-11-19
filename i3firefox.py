616a348890273b08b9cfddeee261eabede2ffce7
Andrei Boghean <andreiboghean00+github@gmail.com>
2024-11-19 19:37:03 +0000

    swap usage of window_class -> .app_id  ; remove workspace_name, window_role from state (not provided in pure wayland)

--- i3firefox.py
+++ i3firefox.py
@@ -42,9 +42,7 @@ from i3ipc.events import Event, WindowEvent  # type: ignore
 @dataclass
 class Window:
     id: Optional[int]
-    window_instance: str
-    window_role: str
-    window_title: str
+    name: str
     workspace_name: Optional[str]
 
 
@@ -68,17 +66,16 @@ class Cache:
         self._closed_windows: List[Window] = []
         self._active_windows = {
             w.id: Window(
-                id=w.id, window_instance=w.window_instance, window_role=w.window_role,
-                window_title=w.window_title, workspace_name=w.workspace().name)
+                id=w.id,
+                name=w.name, workspace_name=w.workspace().name)
             for w in ws}
         self._unknown_windows: Set[int] = set()
+        print(f"{self._active_windows=}")
+        print(f"{self._unknown_windows=}")
 
     def serialize(self) -> str:
-        return json.dumps(
-            [{'instance': w.window_instance,
-              'role': w.window_role,
-              'title': w.window_title,
-              'workspace': w.workspace_name}
+        return json.dumps( [
+            {'title': w.name, 'workspace': w.workspace_name}
              for w in itertools.chain(self._closed_windows, self._active_windows.values())],
             ensure_ascii=False, indent=2)
 
@@ -86,8 +83,8 @@ class Cache:
         if self._active_windows:
             return
         self._closed_windows = [
-            Window(id=None, window_instance=w['instance'], window_role=w['role'],
-                   window_title=w['title'], workspace_name=w['workspace'])
+            Window(id=None,
+                   name=w['title'], workspace_name=w['workspace'])
             for w in json.loads(state)]
 
     def persist(self) -> None:
@@ -106,43 +103,41 @@ class Cache:
             self.deserialize((state_dir/'i3firefox.json').read_text())
 
     def on_window_new(self, i3: Connection, e: WindowEvent) -> None:
-        if e.container.window_class != 'firefox':
+        if e.container.app_id != 'firefox':
             return
 
         self._unknown_windows.add(e.container.id)
         print(f'new unknown {e.container.id}')
         self.persist()
 
-    def on_window_title(self, i3: Connection, e: WindowEvent) -> None:
+    def on_name(self, i3: Connection, e: WindowEvent) -> None:
         con = e.container
-        if con.window_class != 'firefox':
+        if con.app_id != 'firefox':
             return
 
         if con.id in self._unknown_windows:
             if (closed := next((w for w in self._closed_windows
-                                if w.window_instance == con.window_instance
-                                and w.window_role == con.window_role
-                                and w.window_title == con.window_title), None)):
-                print(f'title {con.window_title} last seen on {closed.workspace_name}, moving')
+                                if w.name == con.name), None)):
+                print(f'title {con.name} last seen on {closed.workspace_name}, moving')
                 con.command(f'move --no-auto-back-and-forth container to workspace {closed.workspace_name}')
                 print(f'removing unknown {con.id}')
                 self._unknown_windows.remove(con.id)
-            print(f'new active {con.id} titled {con.window_title}')
+            print(f'new active {con.id} titled {con.name}')
             self._active_windows[con.id] = Window(
-                id=con.id, window_instance=con.window_instance, window_role=con.window_role,
-                window_title=con.window_title,
+                id=con.id,
+                name=con.name,
                 workspace_name=i3.get_tree().find_by_id(con.id).workspace().name)
             self.persist()
             return
 
         if (active := self._active_windows.get(con.id)):
-            print(f'renamed active {con.id} {active.window_title} to {con.window_title}')
-            active.window_title = con.window_title
+            print(f'renamed active {con.id} {active.name} to {con.name}')
+            active.name = con.name
             self.persist()
 
     def on_window_move(self, i3: Connection, e: WindowEvent) -> None:
         con = e.container
-        if con.window_class != 'firefox':
+        if con.app_id != 'firefox':
             return
 
         if con.id in self._unknown_windows:
@@ -156,7 +151,7 @@ class Cache:
 
     def on_window_close(self, i3: Connection, e: WindowEvent) -> None:
         con = e.container
-        if con.window_class != 'firefox':
+        if con.app_id != 'firefox':
             return
 
         if con.id in self._unknown_windows:
@@ -166,21 +161,22 @@ class Cache:
             return
 
         if (was_active := self._active_windows.pop(con.id, None)):
-            print(f'closed active {con.id} {con.window_title} from {was_active.workspace_name}')
+            print(f'closed active {con.id} {con.name} from {was_active.workspace_name}')
             self._closed_windows.append(Window(
-                id=None, window_instance=con.window_instance, window_role=con.window_role,
-                window_title=con.window_title, workspace_name=was_active.workspace_name))
+                id=None,
+                name=con.name, workspace_name=was_active.workspace_name))
             self.persist()
 
 
 def main() -> None:
     conn = Connection()
 
-    cache = Cache(conn.get_tree().find_classed('firefox'))
+    firefoxes = list(filter(lambda x : x.app_id == "firefox", conn.get_tree().descendants()))
+    cache = Cache(firefoxes)
     cache.restore()
 
     conn.on(Event.WINDOW_NEW, cache.on_window_new)
-    conn.on(Event.WINDOW_TITLE, cache.on_window_title)
+    conn.on(Event.WINDOW_TITLE, cache.on_name)
     conn.on(Event.WINDOW_MOVE, cache.on_window_move)
     conn.on(Event.WINDOW_CLOSE, cache.on_window_close)
     conn.main()
